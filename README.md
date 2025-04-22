eaeb8542b720962265dc706b7194ee788a54bb85
SSY199 <s.s.y.9321423@gmail.com>
2025-04-22 20:55:51 +0530

    Server Setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ﻿# Complete_Chap_App
+# npm i express dotenv cors cookie-parser mongoose bcrypt jsonwebtoken
+# npm i nodemon --save-dev
---
+++ server/index.js
@@ -0,0 +1,46 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose, { mongo } from "mongoose";
+import cookieParser from "cookie-parser";
+// import authRoutes from "./routes/auth.route.js";
+// import contactsRoutes from "./routes/contact.route.js";
+// import setupSocket from "./socket.js";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+const databaseURL = process.env.DATABASE_URL;
+
+app.use(
+  cors({
+    origin: [process.env.ORIGIN],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true, // This is crucial
+  })
+);
+
+// app.use("/uploads/profiles", express.static("uploads/profiles"));
+
+app.use(cookieParser());
+app.use(express.json());
+
+// app.use("/api/auth", authRoutes);
+// app.use("/api/contacts", contactsRoutes);
+
+
+const server = app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+// setupSocket(server);
+
+mongoose
+  .connect(databaseURL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
